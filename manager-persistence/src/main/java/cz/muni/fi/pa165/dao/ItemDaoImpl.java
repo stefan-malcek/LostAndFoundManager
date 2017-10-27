3c297d6071ab368f9cc209ec11dc5c55bac44f6e
Adam <bananka.adam@gmail.com>
2017-10-27 23:43:00 +0200

    item entity and dao added

---
+++ manager-persistence/src/main/java/cz/muni/fi/pa165/dao/ItemDaoImpl.java
@@ -0,0 +1,52 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entities.Item;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * Implements {@code ItemDao} interface.
+ *
+ * @author Adam Bananka
+ */
+@Repository
+public class ItemDaoImpl implements ItemDao{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Item cannot be null.");
+        }
+
+        em.persist(item);
+    }
+
+    @Override
+    public void delete(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Item cannot be null.");
+        }
+
+        em.remove(item);
+    }
+
+    @Override
+    public Item findById(long id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Id cannot be lower than or equal to zero");
+        }
+
+        return em.find(Item.class, id);
+    }
+
+    @Override
+    public List<Item> findAll() {
+        return em.createQuery("SELECT i FROM Item i", Item.class).getResultList();
+    }
+}
