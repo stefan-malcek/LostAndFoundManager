0229515f894322489392dbeb6e6ff869bb09c492
Šimon Baláž <simon.balaz96@gmail.com>
2017-11-24 17:18:34 +0100

    Partially implemented Facade and Service for User

---
+++ manager-api/src/main/java/cz/muni/fi/pa165/enums/ItemColor.java
@@ -0,0 +1,18 @@
+package cz.muni.fi.pa165.enums;
+
+/**
+ * Represents a color of item.
+ * @author Adam Bananka
+ */
+public enum ItemColor {
+    Black,
+    Gray,
+    White,
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Orange,
+    Brown,
+    Purple
+}
