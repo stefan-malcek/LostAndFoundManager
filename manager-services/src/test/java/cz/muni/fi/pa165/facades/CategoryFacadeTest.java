251a9e4356eddb4a08b46ea8f722c1bc4c44a450
Stefan Malcek <stefan.malcek1@gmail.com>
2017-11-26 23:46:03 +0100

    Implement simple CategoryFacade tests.

--- manager-services/src/test/java/cz/muni/fi/pa165/facades/CategoryFacadeTest.java
+++ manager-services/src/test/java/cz/muni/fi/pa165/facades/CategoryFacadeTest.java
@@ -3,80 +3,101 @@ package cz.muni.fi.pa165.facades;
 import cz.muni.fi.pa165.ServiceApplicationContext;
 import cz.muni.fi.pa165.dto.CategoryCreateDTO;
 import cz.muni.fi.pa165.dto.CategoryDTO;
-import cz.muni.fi.pa165.entities.Category;
 import cz.muni.fi.pa165.facade.CategoryFacade;
-import cz.muni.fi.pa165.services.CategoryService;
-import org.hibernate.service.spi.ServiceException;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.mockito.Mockito.when;
+import java.util.List;
 
 @ContextConfiguration(classes = ServiceApplicationContext.class)
 public class CategoryFacadeTest extends AbstractTestNGSpringContextTests {
 
-    //TODO: fix mocking the service in the facade. Spring uses a real implementation and not a mock.
-    @Mock
-    private CategoryService categoryService;
-
     @Autowired
-    @InjectMocks
     private CategoryFacade categoryFacade;
 
-    private long electronicsId = 5;
-    private Category electronics;
-    private CategoryCreateDTO electronicsDto;
-
-    @BeforeMethod
-    public void createCategories() {
-        electronics = new Category();
-        electronics.setId(electronicsId);
-        electronics.setName("Electronics");
-        electronics.setDescription("Various lost electronic devices.");
-
-        electronicsDto = new CategoryCreateDTO();
-        electronicsDto.setName("Electronics");
-        electronicsDto.setDescription("Various lost electronic devices.");
-    }
+    private long electronicsId;
+    private String electronics = "Electronics";
+    private CategoryCreateDTO electronicsCreateDTO;
 
     @BeforeClass
-    public void setup() throws ServiceException {
-        MockitoAnnotations.initMocks(this);
+    public void setup() {
+        electronicsCreateDTO = new CategoryCreateDTO();
+        electronicsCreateDTO.setName(electronics);
+        electronicsCreateDTO.setDescription("Various lost electronic devices.");
+    }
+
+    @BeforeMethod
+    public void init() {
+        electronicsId = categoryFacade.createCategory(electronicsCreateDTO);
+    }
+
+    @AfterMethod
+    public void clear() {
+        categoryFacade.remove(electronicsId);
     }
 
     @Test
-    public void update_electronics_updates(){
-        electronicsId = categoryFacade.createCategory(electronicsDto);
+    public void create_electronics_stores() {
+        CategoryCreateDTO toyCreateDTO = new CategoryCreateDTO();
+        toyCreateDTO.setName("Toy");
+        toyCreateDTO.setDescription("Children stuff");
 
+        categoryFacade.createCategory(toyCreateDTO);
+    }
+
+    @Test
+    public void update_category_updates() {
         CategoryDTO category = new CategoryDTO();
         category.setId(electronicsId);
         category.setName("test");
         category.setDescription("Hello");
 
         categoryFacade.updateCategory(category);
-
         CategoryDTO retrieved = categoryFacade.getCategoryById(electronicsId);
 
         Assert.assertEquals(retrieved.getName(), "test");
+        Assert.assertEquals(retrieved.getDescription(), "Hello");
     }
 
     @Test
-    public void getCategoryById_electronicsId_retrievesElectronics(){
-        when(categoryService.findById(electronicsId)).thenReturn(electronics);
+    public void remove_bagId_deletes() {
+        CategoryCreateDTO bagCreateDTO = new CategoryCreateDTO();
+        bagCreateDTO.setName("Bag");
+        bagCreateDTO.setDescription("Items for stuff.");
+        long bagId = categoryFacade.createCategory(bagCreateDTO);
 
-        CategoryDTO category = categoryFacade.getCategoryById(electronicsId);
+        categoryFacade.remove(bagId);
 
-        Assert.assertEquals(category.getId(), electronics.getId());
-        Assert.assertEquals(category.getName(), electronics.getName());
-        Assert.assertEquals(category.getDescription(), electronics.getDescription());
+        CategoryDTO retrieved = categoryFacade.getCategoryById(bagId);
+        Assert.assertNull(retrieved);
     }
 
+    @Test
+    public void getCategoryById_electronicsId_retrieves() {
+        CategoryDTO retrieved = categoryFacade.getCategoryById(electronicsId);
+
+        Assert.assertEquals(retrieved.getName(), electronicsCreateDTO.getName());
+        Assert.assertEquals(retrieved.getDescription(), electronicsCreateDTO.getDescription());
+    }
+
+    @Test
+    public void findByName_electronics_retrieves() {
+        CategoryDTO retrieved = categoryFacade.findByName(electronics);
+
+        Assert.assertEquals(retrieved.getId(), electronicsId);
+        Assert.assertEquals(retrieved.getDescription(), electronicsCreateDTO.getDescription());
+    }
+
+    @Test
+    public void getAllCategories_retrievesCategories() {
+        List<CategoryDTO> categories = categoryFacade.getAllCategories();
+
+        Assert.assertTrue(categories.size() == 1 || categories.size() == 2);
+    }
 }
