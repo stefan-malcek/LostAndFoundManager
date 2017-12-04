abf2754f61a2426d8efe69b6abd3e588106149a0
adambananka <addame121@gmail.com>
2017-12-04 18:21:40 +0100

    EventFacade exception handling added

--- manager-services/src/main/java/cz/muni/fi/pa165/facades/EventFacadeImpl.java
+++ manager-services/src/main/java/cz/muni/fi/pa165/facades/EventFacadeImpl.java
@@ -12,6 +12,7 @@ import cz.muni.fi.pa165.services.EventService;
 import cz.muni.fi.pa165.services.ItemService;
 import cz.muni.fi.pa165.services.MappingService;
 import cz.muni.fi.pa165.services.UserService;
+import exceptions.LostAndFoundManagerDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,116 +43,174 @@ public class EventFacadeImpl implements EventFacade {
 
     @Override
     public long createEvent(EventDTO eventDTO) {
-        Event event = mappingService.mapTo(eventDTO, Event.class);
-        eventService.createEvent(event);
-        return event.getId();
+        try {
+            Event event = mappingService.mapTo(eventDTO, Event.class);
+            eventService.createEvent(event);
+            return event.getId();
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Event cannot be created.", ex);
+        }
     }
 
     @Override
     public void deleteEvent(long eventId) {
-        Event event = eventService.findEventById(eventId);
-        eventService.deleteEvent(event);
+        try {
+            Event event = eventService.findEventById(eventId);
+            eventService.deleteEvent(event);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Event cannot be deleted.", ex);
+        }
     }
 
     @Override
     public long addLoosing(EventLossDTO eventDTO) {
-        Event event = eventService.findEventById(eventDTO.getId());
-        if (event == null) {
-            event = mappingService.mapTo(eventDTO, Event.class);
-            eventService.createEvent(event);
+        try {
+            if (eventDTO.getId() <= 0) {
+                Event event = mappingService.mapTo(eventDTO, Event.class);
+                eventService.createEvent(event);
+                return event.getId();
+            }
+
+            Event event = eventService.findEventById(eventDTO.getId());
+            User owner = mappingService.mapTo(eventDTO.getOwner(), User.class);
+            eventService.addLoosing(event, owner, eventDTO.getDateOfLoss(), eventDTO.getPlaceOfLoss());
             return event.getId();
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Loosing cannot be added to event.", ex);
         }
-        User owner = mappingService.mapTo(eventDTO.getOwner(), User.class);
-        eventService.addLoosing(event, owner, eventDTO.getDateOfLoss(), eventDTO.getPlaceOfLoss());
-        return event.getId();
     }
 
     @Override
     public long addFinding(EventFindDTO eventDTO) {
-        Event event = eventService.findEventById(eventDTO.getId());
-        if (event == null) {
-            event = mappingService.mapTo(eventDTO, Event.class);
-            eventService.createEvent(event);
+        try {
+            if (eventDTO.getId() <= 0) {
+                Event event = mappingService.mapTo(eventDTO, Event.class);
+                eventService.createEvent(event);
+                return event.getId();
+            }
+
+            Event event = eventService.findEventById(eventDTO.getId());
+            User finder = mappingService.mapTo(eventDTO.getFinder(), User.class);
+            eventService.addFinding(event, finder, eventDTO.getDateOfFind(), eventDTO.getPlaceOfFind());
             return event.getId();
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Finding cannot be added to event.", ex);
         }
-        User finder = mappingService.mapTo(eventDTO.getFinder(), User.class);
-        eventService.addFinding(event, finder, eventDTO.getDateOfFind(), eventDTO.getPlaceOfFind());
-        return event.getId();
     }
 
     @Override
     public boolean checkEventResolved(long eventId) {
-        Event event = eventService.findEventById(eventId);
-        return eventService.checkEventResolved(event);
+        try {
+            Event event = eventService.findEventById(eventId);
+            return eventService.checkEventResolved(event);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Event cannot be checked.", ex);
+        }
     }
 
     @Override
     public EventDTO findEventById(long eventId) {
-        Event event = eventService.findEventById(eventId);
-        return event == null
-                ? null
-                : mappingService.mapTo(event, EventDTO.class);
+        try {
+            Event event = eventService.findEventById(eventId);
+            return event == null
+                    ? null
+                    : mappingService.mapTo(event, EventDTO.class);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Cannot find event by id.", ex);
+        }
     }
 
     @Override
     public EventDTO findEventByItem(long itemId) {
-        Item item = itemService.findById(itemId);
-        if (item == null) {
-            return null;
+        try {
+            Item item = itemService.findById(itemId);
+            if (item == null) {
+                return null;
+            }
+            Event event = eventService.findEventByItem(item);
+            return event == null
+                    ? null
+                    : mappingService.mapTo(event, EventDTO.class);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Cannot find event by item.", ex);
         }
-        Event event = eventService.findEventByItem(item);
-        return event == null
-                ? null
-                : mappingService.mapTo(event, EventDTO.class);
     }
 
     @Override
     public List<EventDTO> findAllEvents() {
-        List<Event> events = eventService.findAllEvents();
-        return mappingService.mapTo(events, EventDTO.class);
+        try {
+            List<Event> events = eventService.findAllEvents();
+            return mappingService.mapTo(events, EventDTO.class);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Cannot find all events.", ex);
+        }
     }
 
     @Override
     public List<EventDTO> findEventsByFinder(long finderId) {
-        User user = userService.findUserById(finderId);
-        if (user == null) {
-            return null;
+        try {
+            User user = userService.findUserById(finderId);
+            if (user == null) {
+                return null;
+            }
+            List<Event> events = eventService.findEventsByFinder(user);
+            return mappingService.mapTo(events, EventDTO.class);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Cannot find events by finder.", ex);
         }
-        List<Event> events = eventService.findEventsByFinder(user);
-        return mappingService.mapTo(events, EventDTO.class);
     }
 
     @Override
     public List<EventDTO> findEventsByOwner(long ownerId) {
-        User user = userService.findUserById(ownerId);
-        if (user == null) {
-            return null;
+        try {
+            User user = userService.findUserById(ownerId);
+            if (user == null) {
+                return null;
+            }
+            List<Event> events = eventService.findEventsByOwner(user);
+            return mappingService.mapTo(events, EventDTO.class);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Cannot find events by owner.", ex);
         }
-        List<Event> events = eventService.findEventsByOwner(user);
-        return mappingService.mapTo(events, EventDTO.class);
     }
 
     @Override
     public List<EventDTO> findEventsByPlaceOfLoss(String place) {
-        List<Event> events = eventService.findEventsByPlaceOfLoss(place);
-        return mappingService.mapTo(events, EventDTO.class);
+        try {
+            List<Event> events = eventService.findEventsByPlaceOfLoss(place);
+            return mappingService.mapTo(events, EventDTO.class);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Cannot find events by place of loss.", ex);
+        }
     }
 
     @Override
     public List<EventDTO> findEventsByPlaceOfFind(String place) {
-        List<Event> events = eventService.findEventsByPlaceOfFind(place);
-        return mappingService.mapTo(events, EventDTO.class);
+        try {
+            List<Event> events = eventService.findEventsByPlaceOfFind(place);
+            return mappingService.mapTo(events, EventDTO.class);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Cannot find events by place of find.", ex);
+        }
     }
 
     @Override
     public List<EventDTO> findEventsByDateOfFind(Date date) {
-        List<Event> events = eventService.findEventsByDateOfFind(date);
-        return mappingService.mapTo(events, EventDTO.class);
+        try {
+            List<Event> events = eventService.findEventsByDateOfFind(date);
+            return mappingService.mapTo(events, EventDTO.class);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Cannot find events by date of find.", ex);
+        }
     }
 
     @Override
     public List<EventDTO> findEventsByDateOfLoss(Date date) {
-        List<Event> events = eventService.findEventsByDateOfLoss(date);
-        return mappingService.mapTo(events, EventDTO.class);
+        try {
+            List<Event> events = eventService.findEventsByDateOfLoss(date);
+            return mappingService.mapTo(events, EventDTO.class);
+        } catch (Exception ex) {
+            throw new LostAndFoundManagerDataAccessException("Cannot find events by date of loss.", ex);
+        }
     }
 }
