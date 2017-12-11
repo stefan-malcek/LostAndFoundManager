96a5d092575a4314b8aabfd254a7ee8fd4d9f0b3
Stefan Malcek <stefan.malcek1@gmail.com>
2017-12-11 22:28:59 +0100

    Implement a simple REST controller for item.

---
+++ manager-web/src/main/java/cz/muni/fi/pa165/web/rest/ApiUris.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.web.rest;
+
+/**
+ *
+ * @author stefa
+ */
+public abstract class ApiUris {
+    public static final String ROOT_URI_ITEMS = "/items";  
+}
